8c6efd52ccafae04f3295a275b65f066a959ea2d
Connor Ocampo <connorocampo@gmail.com>
2021-10-03 08:49:48 -0400

    Remove: restricted mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react'
-import TodoItem from './components/TodoItem'
-import fakeData from './fakeData'
+import React from "react"
+import TodoItem from "./TodoItem"
+import todosData from "./todosData"
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      data: fakeData
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
     }
-  }
-  render() {
-    const TodoArray = this.state.data.map(item => <TodoItem key={item.id} item={item}/>)
-    return (
-      <div className="todo-list">
-        <h1>Front-end Engineer Dream Stack</h1>
-        <p style={{marginTop: '10px', marginBottom:'3em'}}>Technologies that I'm most interested in working with!</p>
-        {TodoArray}
-      </div>
-    );
-  }
+    
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+    
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+        
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )    
+    }
 }
 
-export default App;
+export default App
