ef52f7cd23e64a9a069d33b663178f2fe24c34b6
Connor Ocampo <connorocampo@gmail.com>
2021-10-06 11:47:54 -0400

    chore: add log out styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import todosData from "./todosData"
 import TodoItem from "./components/TodoItem"
 import Conditional from './components/Conditional'
 
+// Images
+import Vegas from './assets/images/vegas.jpg'
+import VegasShadow from './assets/images/vegas-shadow.jpg'
+
 class App extends React.Component {
     constructor() {
         super()
@@ -44,27 +48,46 @@ class App extends React.Component {
     render() {
         const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
 
+        const buttonText = this.state.isLoggedIn ? "Log Out" : "Log In"
+
         const buttonStyle = {
             background: 'black'
         }
-        
-        const buttonText = this.state.isLoggedIn ? "Log Out" : "Log In"
+
+        const backgroundStyle = {
+            backgroundImage: `url(${VegasShadow})`,
+            backgroundPosition: 'contain',
+            height: '100vh'
+            // backgroundRepeat: 'no-repeat'
+        }
+
+        const whiteTextStyle = {
+            color: 'white'
+        }
+
+        const blackTextStyle = {
+            color: `var(--black)`
+        }
         
 
         return (
-          <div>
-              <header>
-                  <p id="site-logo">The Las Vegas To Do List</p>
-                  <button id="log-btn" onClick={this.logClick} style={this.state.isLoggedIn ? buttonStyle : null}>{buttonText}</button>
-              </header>
+          <div style={this.state.isLoggedIn ? null : backgroundStyle}>
 
-              {this.state.isLoggedIn ?
-              <div className="todo-list">
-                  <h1 style={{marginBottom: '25px'}}>Things To Do</h1>
-                {todoItems}
-            </div>:
+            <header>
+                <p id="site-logo">The Las Vegas To Do List</p>
+                <button id="log-btn" onClick={this.logClick} style={this.state.isLoggedIn ? buttonStyle : null}>{buttonText}</button>
+            </header>
+
+            {this.state.isLoggedIn ?
+                <div className="todo-list" style={{backgroundImage: `url(${Vegas})`, backgroundPosition: 'center'}}>
+                    <h1 style={{marginBottom: '25px', fontFamily: 'Oswald, sans-serif'}}>Things To Do</h1>
+                    {todoItems}
+                </div>:
             <Conditional />}
-
+            
+            <footer>
+                <p style={this.state.isLoggedIn ? blackTextStyle : whiteTextStyle}>The Las Vegas To Do List | Made by Connor Ocampo</p>
+            </footer>
           </div>
         )    
     }
--- src/components/Conditional.js
+++ src/components/Conditional.js
@@ -1,6 +1,12 @@
+// Images
+import VegasShadow from '../assets/images/vegas-shadow.jpg'
+
 function Conditional(props) {
     return (
-        <h1 style={{textAlign: 'center', marginTop: '24px'}}>Logged Out Conditional Component</h1>
+        <div className="hero-logged-out">
+            <h1 style={{fontFamily: 'Oswald, sans-serif', fontSize: '64px', color: `var(--red)`, textTransform: 'uppercase'}}>Las Vegas Trip 2021</h1>
+            <p style={{maxWidth: '400px', marginTop: '0', fontSize: '24px', lineHeight: '1.25', color: 'white'}}>This year I got to treat my mom to a Las Vegas vacation. Log in to view what’s on our Vegas To Do List! (No sign up required.)</p>
+        </div>
     )
 }
 
