38e8fde3aae9065de392f18ae942766ed90c7e7b
Connor Ocampo <connorocampo@gmail.com>
2021-12-06 10:01:51 -0500

    Chore: update footer name

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends React.Component {
                 </div>
                 
                 <footer style={this.state.isLoggedIn ? blackTextStyle : whiteTextStyle}>
-                    <p>Made by Connor Ocampo</p>
+                    <p>Made by Casey Ocampo</p>
                 </footer>
             </div>
         )    
